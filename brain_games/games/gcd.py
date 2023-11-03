69352a5fe474a6a2e27bf17d819fcf75d2a55456
Владислав Любимов <lyubimov.vladislav13@gmail.com>
2023-11-03 11:07:48 +0300

    Some changes

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,17 +1,21 @@
 import random
-from math import gcd
+
 
 rules = 'Find the greatest common divisor of given numbers.'
 
 
-def hcf(first_num, second_num):
-    result = gcd(first_num, second_num)
-    return result
+def gcd(number1, number2):
+    while number1 != 0 and number2 != 0:
+        if number1 >= number2:
+            number1 %= number2
+        else:
+            number2 %= number1
+    return str(number1 or number2)
 
 
 def generation(number_min, number_max):
     first_num = random.randint(number_min, number_max)
     second_num = random.randint(number_min, number_max)
     union = f'{first_num} {second_num}'
-    correct_answer = str(hcf(first_num, second_num))
+    correct_answer = gcd(first_num, second_num)
     return union, correct_answer
